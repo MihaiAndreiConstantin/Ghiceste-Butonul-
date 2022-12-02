1939b5427a097c184250812651ca2420d3650875
MihaiAndreiConstantin <117904698+MihaiAndreiConstantin@users.noreply.github.com>
2022-12-02 17:04:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,49 +5,16 @@ let buttonsPressed = 0
 function randomNumbers() {
     luckyNumber = Math.floor(Math.random() * 3);
     arr[luckyNumber] = "Lucky"
-    console.log(luckyNumber)
 }
 
-function unlucky() {
-     document.getElementById("luck-test").innerHTML = "Better luck next time!"
-}
-
-function lucky() {
-     document.getElementById("luck-test").innerHTML = "You are lucky, you should buy some lotto tickets."
-}
-
-function pressFirstButton() {
+function pressButton(x) {
     if (buttonsPressed == 0) {
         ++buttonsPressed
-        document.getElementById("change-text1").innerHTML = arr[0]
-        if (arr[0] == "Lucky") {
-            lucky()
+        document.getElementById(x).innerHTML = arr[x]
+        if (arr[x] == "Lucky") {
+            document.getElementById("luck-test").innerHTML = "You are lucky, you should buy some lotto tickets."
         } else {
-            unlucky()
-        }
-    }
-}
-
-function pressSecondButton() {
-    if (buttonsPressed == 0) {
-        ++buttonsPressed
-        document.getElementById("change-text2").innerHTML = arr[1]
-        if (arr[1] == "Lucky") {
-            lucky()
-        } else {
-            unlucky()
-        }
-    }
-}
-
-function pressThirdButton() {
-    if (buttonsPressed == 0) {
-        ++buttonsPressed
-        document.getElementById("change-text3").innerHTML = arr[2]
-        if (arr[2] == "Lucky") {
-            lucky()
-        } else {
-            unlucky()
+            document.getElementById("luck-test").innerHTML = "Better luck next time!"
         }
     }
 }
